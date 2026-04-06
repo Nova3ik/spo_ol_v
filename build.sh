0d09f6ab0a6f51cd373eb37c15370dc2fcfe2a81
NikitosCLS63 <sirex.xumuk@gmail.com>
2026-04-06 21:54:15 +0300

    finalfix

--- build.sh
+++ build.sh
@@ -6,6 +6,14 @@ python -m pip install --upgrade pip
 python -m pip install --no-cache-dir -r requirements.txt
 python manage.py collectstatic --no-input
 
+rm -rf staticfiles_build
+mkdir -p staticfiles_build/static
+
+if [ -d static ]; then
+  cp -R static/. staticfiles_build/static/
+fi
+
 if [ -d staticfiles/admin ]; then
-  cp -R staticfiles/admin static/
+  mkdir -p staticfiles_build/static/admin
+  cp -R staticfiles/admin/. staticfiles_build/static/admin/
 fi
