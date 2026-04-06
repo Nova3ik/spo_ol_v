510b7db099309823801c631dfa42845d199a3558
NikitosCLS63 <sirex.xumuk@gmail.com>
2026-04-06 21:41:56 +0300

    bigfix

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
+set -e
+
 python -m venv .vercel-venv
 . .vercel-venv/bin/activate
 python -m pip install --upgrade pip
 python -m pip install --no-cache-dir -r requirements.txt
 python manage.py collectstatic --no-input
-mkdir -p staticfiles_build
-cp -R staticfiles staticfiles_build/static
-python manage.py migrate
+
+if [ -d staticfiles/admin ]; then
+  cp -R staticfiles/admin static/
+fi
--- config/settings.py
+++ config/settings.py
@@ -52,7 +52,7 @@ else:
     env = SimpleEnv()
 
 SECRET_KEY = env("SECRET_KEY", default="dev-insecure-key-change-me")
-DEBUG = env.bool("DEBUG", default=True)
+DEBUG = env.bool("DEBUG", default=False)
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["127.0.0.1", "localhost"])
 
 INSTALLED_APPS = [
@@ -124,7 +124,7 @@ TIME_ZONE = "Europe/Moscow"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "/media/"
@@ -139,5 +139,4 @@ ALLOWED_HOSTS = ['*']
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
 
